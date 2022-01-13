ca3ae92d2d697a2a423b65e67d83ec2f1400e56f
Rajvardhan Patil <rajbpatil8623@gmail.com>
2022-01-13 13:07:40 +0530

    JAVA FILES

---
+++ FeatureList.java
@@ -0,0 +1,8 @@
+package Study;
+
+public interface FeatureList {
+
+	public double add(double a , double b);
+	
+	public int Div(int a , int b);
+}
---
+++ MySuperCalc.java
@@ -0,0 +1,18 @@
+package Study;
+
+public class MySuperCalc implements FeatureList{
+
+	@Override
+	public double add(double a, double b) {
+		return a+b;
+	}
+
+	@Override
+	public int Div(int a, int b) {
+	
+		return a/b;
+	}
+	
+
+	
+}
